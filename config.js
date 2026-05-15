77adc432756e2e4a6d689fbbe8227bdb87f5ec03
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-05-15 11:05:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ export const emailConfig = {
 };
 
 export const aiConfig = {
-    geminiKey: "AIzaSyD397oBwR17NuMzgcNrWHYcxFRtoDokCaE"
+    geminiKey: "AIzaSyC7j-lCZZFENaBA-zCdawhqZF1p9MKWfeM"
 };
