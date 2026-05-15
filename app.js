347d22d321cfdc76e5f1120791a850b2e43b34bd
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-05-15 11:01:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, signOut, GoogleAuthProvider, signInWithPopup, updatePassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, updateDoc, increment, deleteDoc, arrayUnion, arrayRemove, serverTimestamp, onSnapshot, orderBy, limit } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { firebaseConfig, emailConfig } from "./config.js";
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const provider = new GoogleAuthProvider();
+
+emailjs.init(emailConfig.publicKey);
+
+export { 
+    doc, setDoc, getDoc, collection, addDoc, query, where, getDocs, updateDoc, 
+    increment, deleteDoc, arrayUnion, arrayRemove, serverTimestamp, onSnapshot, orderBy, limit,
+    signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged, 
+    signOut, signInWithPopup, updatePassword 
+};
+
+export function getThumbnail(url) {
+    try {
+        let id = '';
+        if(url.includes('v=')) id = url.split('v=')[1].split('&')[0];
+        else if(url.includes('youtu.be/')) id = url.split('/').pop().split('?')[0];
+        else if(url.includes('/shorts/')) id = url.split('/shorts/')[1].split('?')[0];
+        else if(url.includes('/live/')) id = url.split('/live/')[1].split('?')[0];
+        return id ? `https://img.youtube.com/vi/${id}/mqdefault.jpg` : 'https://res.cloudinary.com/dowhvdkjh/image/upload/v1777895769/IMG-20260504-WA0002_fucbjd.jpg';
+    } catch(e) { return 'https://res.cloudinary.com/dowhvdkjh/image/upload/v1777895769/IMG-20260504-WA0002_fucbjd.jpg'; }
+}
+
+export async function sendOTP(email, code) {
+    return await emailjs.send(emailConfig.serviceID, emailConfig.templateID, { to_email: email, otp: code });
+}
---
+++ config.js
@@ -0,0 +1,20 @@
+// OpenClass Security Configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyAAtk7rWxT7VFKA6J1UVHicHSakceMc4TA",
+    authDomain: "openclass-565ca.firebaseapp.com",
+    projectId: "openclass-565ca",
+    storageBucket: "openclass-565ca.firebasestorage.app",
+    messagingSenderId: "43042586871",
+    appId: "1:43042586871:web:d35842d6f67d44c24fd3f2",
+    measurementId: "G-BR2YDKX6B4"
+};
+
+export const emailConfig = {
+    serviceID: "service_djr1n0f",
+    templateID: "template_tiqnb53",
+    publicKey: "P88qoYcr5CHYVNAPw"
+};
+
+export const aiConfig = {
+    geminiKey: "AIzaSyD397oBwR17NuMzgcNrWHYcxFRtoDokCaE"
+};
