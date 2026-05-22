fef4a2aa81f3ff9a7a741844049b0ab497df4b76
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-05-22 11:02:05 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,18 @@
 const CACHE_NAME = 'openclass-v1';
 const ASSETS = [
-    '/',
-    '/index.html',
-    '/app.js',
-    '/config.js'
+  '/',
+  '/index.html',
+  '/app.js',
+  '/config.js',
+  'https://cdn.tailwindcss.com'
 ];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
