99ea780ed5971924abf67cd7d2decc7733e2702b
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-06-02 15:39:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const adminDashboard =
 // OPEN DASHBOARD
 // =========================================
 
-adminBtn.addEventListener('click', () => {
+AdminButton.addEventListener('click', () => {
 
     adminDashboard.classList.remove('hidden');
 
